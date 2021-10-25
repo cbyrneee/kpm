59e313fc96ca112de75326918815ade30e3d686b
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-10-25 21:33:42 +0100

    Launch: Use clikt for parsing arguments

--- src/main/kotlin/dev/cbyrne/kpm/main.kt
+++ src/main/kotlin/dev/cbyrne/kpm/main.kt
@@ -1,11 +1,24 @@
 package dev.cbyrne.kpm
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
 import kotlin.io.path.Path
 
-fun main() {
+class KPMCommand : CliktCommand() {
+    private val scriptPath: String? by option(help = "Define a custom path to your kpm script")
+
+    override fun run() {
+        val path = Path(scriptPath ?: "kpm.kts")
+        println("[kpm] Loading project from $path...")
+
+        val kpm = KPM.load(path)
+        println("[kpm] Initializing project ${kpm.currentProject.script.name}...")
+        println("[kpm] Project ${kpm.currentProject.script.name} initialized.")
+    }
+}
+
+fun main(args: Array<String>) {
     // FIXME: Only used for debugging purposes, see https://youtrack.jetbrains.com/issue/KT-40937
     System.setProperty("idea.io.use.nio2", "true")
-
-    val kpm = KPM.load(Path("kpm.kts"))
-    println("[kpm] Loading project ${kpm.currentProject.script.name}...")
+    KPMCommand().main(args)
 }
