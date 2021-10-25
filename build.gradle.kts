5d3ca837d2119228c1e5adc4a4705a2c88a4826a
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-10-25 22:12:39 +0100

    Build: Add kotlinx-serialization to dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.31"
+    kotlin("plugin.serialization") version "1.5.31"
 }
 
 group = "dev.cbyrne.kpm"
@@ -15,6 +16,8 @@ dependencies {
     implementation(kotlin("script-runtime"))
     implementation(kotlin("compiler-embeddable"))
     implementation(kotlin("scripting-compiler-embeddable"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
 
     implementation("com.github.ajalt.clikt:clikt:3.3.0")
 }
