faa59562e6a9c2646a5c51721175e65e6ef1d890
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2021-10-26 16:27:20 +0100

    Core: Remove @Serializable annotation from Project and ProjectScript

--- src/main/kotlin/dev/cbyrne/kpm/project/Project.kt
+++ src/main/kotlin/dev/cbyrne/kpm/project/Project.kt
@@ -2,9 +2,7 @@ package dev.cbyrne.kpm.project
 
 import dev.cbyrne.kpm.KPM
 import dev.cbyrne.kpm.project.pkg.ProjectScript
-import kotlinx.serialization.Serializable
 
-@Serializable
 data class Project(val script: ProjectScript) {
     companion object {
         fun parse(script: String) = Project(KPM.scriptManager.parse(script))
--- src/main/kotlin/dev/cbyrne/kpm/project/pkg/ProjectScript.kt
+++ src/main/kotlin/dev/cbyrne/kpm/project/pkg/ProjectScript.kt
@@ -1,6 +1,3 @@
 package dev.cbyrne.kpm.project.pkg
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class ProjectScript(val name: String)
